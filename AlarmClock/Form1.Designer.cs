e4c2db342a0a5d046d8f9f646211681c745a6628
atereshhenko <atereshhenko@gmail.com>
2020-05-26 08:42:27 +0300

    Download project

---
+++ AlarmClock/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace AlarmClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnОк = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuHideShow = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Сейчас";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "00:00:00";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnОк);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 46);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(140, 134);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Сигнал";
+            // 
+            // btnОк
+            // 
+            this.btnОк.Location = new System.Drawing.Point(6, 94);
+            this.btnОк.Name = "btnОк";
+            this.btnОк.Size = new System.Drawing.Size(128, 23);
+            this.btnОк.TabIndex = 5;
+            this.btnОк.Text = "Ок";
+            this.btnОк.UseVisualStyleBackColor = true;
+            this.btnОк.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 71);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(75, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "вкл/выкл";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(69, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "минут";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(6, 45);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(57, 20);
+            this.numericUpDown2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "часов";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(6, 19);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(57, 20);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuHideShow,
+            this.toolStripSeparator2,
+            this.toolStripMenuAbout,
+            this.toolStripSeparator1,
+            this.toolStripMenuExit});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(173, 82);
+            // 
+            // toolStripMenuHideShow
+            // 
+            this.toolStripMenuHideShow.Name = "toolStripMenuHideShow";
+            this.toolStripMenuHideShow.Size = new System.Drawing.Size(172, 22);
+            this.toolStripMenuHideShow.Text = "Hide/Show aClock";
+            this.toolStripMenuHideShow.Click += new System.EventHandler(this.toolStripMenuHideShow_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(169, 6);
+            // 
+            // toolStripMenuAbout
+            // 
+            this.toolStripMenuAbout.Name = "toolStripMenuAbout";
+            this.toolStripMenuAbout.Size = new System.Drawing.Size(172, 22);
+            this.toolStripMenuAbout.Text = "About";
+            this.toolStripMenuAbout.Click += new System.EventHandler(this.toolStripMenuAbout_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(169, 6);
+            // 
+            // toolStripMenuExit
+            // 
+            this.toolStripMenuExit.Name = "toolStripMenuExit";
+            this.toolStripMenuExit.Size = new System.Drawing.Size(172, 22);
+            this.toolStripMenuExit.Text = "Exit";
+            this.toolStripMenuExit.Click += new System.EventHandler(this.toolStripMenuExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(165, 192);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AlarmClock";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnОк;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuExit;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuHideShow;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuAbout;
+    }
+}
+
---
+++ AlarmClock/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Media;
+
+namespace AlarmClock
+{
+    public partial class Form1 : Form
+    {
+		private DateTime alarm;
+		SoundPlayer sp = new SoundPlayer();	
+		public Form1()
+        {
+            InitializeComponent();
+			
+			numericUpDown1.Maximum = 23;
+			numericUpDown1.Minimum = 0;
+
+			numericUpDown2.Maximum = 59;
+			numericUpDown2.Minimum = 0;
+
+			numericUpDown1.Value = DateTime.Now.Hour;
+			numericUpDown2.Value = DateTime.Now.Minute;
+					
+			notifyIcon1.Text = "Будильник не установлен";
+			notifyIcon1.Visible = true;
+			
+			timer1.Interval = 1000;
+			timer1.Enabled = true;
+
+			label2.Text = DateTime.Now.ToLongTimeString();
+		}
+
+        private void toolStripMenuExit_Click(object sender, EventArgs e)
+        {
+			this.Close();
+        }
+
+        private void toolStripMenuHideShow_Click(object sender, EventArgs e)
+        {
+			if (this.Visible) this.Hide();
+			else this.Show();
+		}
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+			sp.Stop();
+			if (checkBox1.Checked)
+			{
+				numericUpDown1.Enabled = false;
+				numericUpDown2.Enabled = false;
+				alarm = new DateTime(
+					DateTime.Now.Year,
+					DateTime.Now.Month,
+					DateTime.Now.Day,
+					Convert.ToInt16(numericUpDown1.Value),
+					Convert.ToInt16(numericUpDown2.Value),
+					0, 0);
+				if (DateTime.Compare(DateTime.Now, alarm) > 0)
+					alarm = alarm.AddDays(1);
+
+				notifyIcon1.Text = "Будильник: " + alarm.ToShortTimeString();
+			}
+			else
+			{
+				numericUpDown1.Enabled = true;
+				numericUpDown2.Enabled = true;
+				notifyIcon1.Text = "Будильник не установлен";
+			}
+		}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+			this.Hide();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+			label2.Text = DateTime.Now.ToLongTimeString();
+			if (checkBox1.Checked)
+			{
+				// будильник установлен
+				if (DateTime.Compare(DateTime.Now, alarm) > 0)
+				{
+					checkBox1.Checked = false;							
+					sp.Stream = Properties.Resources.ring;
+					sp.Play();
+					this.Show();
+				}
+			}
+		}
+
+        private void toolStripMenuAbout_Click(object sender, EventArgs e)
+        {
+			MessageBox.Show("Программа AlarmClock.\n\n" +
+				"Простой будильник.\n\n" +
+				"Сделано в Microsoft Visual Studio C#\n" +
+				"for the Microsoft® .NET Framework\n",
+				"AlarmClock " + GetType().Assembly.GetName().Version.ToString(),
+				MessageBoxButtons.OK,
+				MessageBoxIcon.Information);
+		}
+    }
+}
